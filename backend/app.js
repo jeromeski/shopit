deecc29e756880890833a559db06e0c71697b180
jeromeski <jeromegee@gmail.com>
2021-02-19 10:19:29 +0800

    Creating first route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const products = require('./routes/product');
+
+//  middlewares here
+app.use(morgan('dev'));
+app.use(express.json());
+
+// import all routes here
+app.use('/api/v1', products);
+
 module.exports = app
---
+++ backend/routes/product.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { getProducts } = require('../controllers/productControllers');
+
+router.route('/products').get(getProducts);
+
+module.exports = router;
