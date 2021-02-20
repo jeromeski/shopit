8dc97ef1dc3564a26c653b05f7332685b0ad8978
jeromeski <jeromegee@gmail.com>
2021-02-20 17:05:35 +0800

    Display all Products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -11,9 +11,11 @@ exports.newProduct = async (req, res, next) => {
 };
 
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+	const products = await Product.find({});
 	res.status(200).json({
-		success: true,
-		message: 'This route will show all products in database'
+		sucess: true,
+		count: products.length,
+		products
 	});
 };
