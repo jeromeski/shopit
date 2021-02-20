25cb050b25e37e3cbf564ae66e74ce299b925714
jeromeski <jeromegee@gmail.com>
2021-02-20 20:30:13 +0800

    Admin- Update Product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productControllers');
+const {
+	getProducts,
+	newProduct,
+	getSingleProduct,
+	updateProduct
+} = require('../controllers/productControllers');
 
-router.route('/product/new').post(newProduct);
+
 
 // router.route('/products').get(getProducts);
 router.get('/products', getProducts);
 
 router.get('/product/:id', getSingleProduct);
+
+router.route('/admin/product/:id').put(updateProduct);
+
+router.route('/admin/product/new').post(newProduct);
 
 module.exports = router;
